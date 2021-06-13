6a3494dc204e1a41edb0e9002640c5a2b39f7bef
Oskar <oskarek.i.tomirek@gmail.com>
2021-06-13 15:44:06 +0200

    First Triangle constructor

--- src/main/java/tk/chaber/triangles_properties/Geometry/Triangle.java
+++ src/main/java/tk/chaber/triangles_properties/Geometry/Triangle.java
@@ -8,7 +8,21 @@ public class Triangle {
                     B,
                     C;
 
-    public Triangle(){
+    public Triangle(Section c, Angle alpha, Angle beta){
+        Point   A = c.getA(),
+                B = c.getB();
 
+        Straight    bStraight = new Straight(A, alpha),
+                    aStraight = new Straight(B, beta);
+
+        Point   C = bStraight.findIntersection(aStraight);
+
+        Section b = new Section(A, C),
+                a = new Section(B, C);
+
+
+        this.a = a;
+        this.b = b;
+        this.c = c;
     }
 }
