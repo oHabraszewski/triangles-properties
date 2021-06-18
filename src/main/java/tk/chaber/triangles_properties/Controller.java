4e981aa4a852faf32fc0891806792fde4ea08e84
Oskar <oskarek.i.tomirek@gmail.com>
2021-06-18 07:58:41 +0200

    Controller changes, start of data handling

--- src/main/java/tk/chaber/triangles_properties/Controller.java
+++ src/main/java/tk/chaber/triangles_properties/Controller.java
@@ -1,10 +1,37 @@
 package tk.chaber.triangles_properties;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import tk.chaber.triangles_properties.Geometry.Angle;
+import tk.chaber.triangles_properties.Geometry.Point;
+import tk.chaber.triangles_properties.Geometry.Section;
+import tk.chaber.triangles_properties.Geometry.Triangle;
+
 
 public class Controller {
     @FXML
-    public void createTriangle(){
+    private TextField   input1,
+                        input2,
+                        input3;
+
+    @FXML
+    private Text side1,
+                    side2,
+                    side3;
+    @FXML
+    public void createTriangle(ActionEvent event){
+        double  value1 = Double.parseDouble(input1.getText()),
+                value2 = Double.parseDouble(input2.getText()),
+                value3 = Double.parseDouble(input3.getText());
+
+        Section sideC = new Section(new Point(0, 0), new Angle(0), value1);
+        Angle   alpha = new Angle(value2),
+                beta = new Angle(180 - value3);
+        Triangle triangle = new Triangle(sideC, alpha, beta);
+        System.out.println(triangle.toString());
 
     }
 }
--- src/main/java/tk/chaber/triangles_properties/Geometry/Angle.java
+++ src/main/java/tk/chaber/triangles_properties/Geometry/Angle.java
@@ -4,7 +4,7 @@ public class Angle {
     private double radians;
     private double degrees;
 
-    public Angle(int degrees){
+    public Angle(double degrees){
         this.degrees = degrees;
         this.radians = degrees * Math.PI/180;
     }
